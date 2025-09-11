bee5b1871def7cdd915ee629f62d714496b7b93f
Oleksandr Dmukhovskyi <olexandrbig@gmail.com>
2025-09-11 15:39:21 +0300

    fix main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 import Landing from "@/components/landing";
+import {Metadata} from "next";
 
-export const metadata = {
-  title: "SBOM to NOTICE | Local-first builder",
+export const metadata: Metadata = {
+  title: "SBOM to NOTICE",
   description:
-    "Generate and edit NOTICE files from your SBOM entirely in the browser. No uploads. GDPR-friendly by design.",
-  openGraph: {
-    title: "SBOM to NOTICE | Local-first builder",
-    description:
-      "Generate and edit NOTICE files from your SBOM entirely in the browser. No uploads. GDPR-friendly by design.",
-    siteName: "SBOM to NOTICE",
-    locale: "en_US",
-    type: "website"
-  }
+    "Generate and edit NOTICE files from your SBOM entirely in the browser. No uploads. GDPR-friendly by design."
 };
 
 export default function Page() {
